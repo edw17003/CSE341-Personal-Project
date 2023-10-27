81cc40a7038dbfdeb42056e5b204f3427e113b1e
edw17003 <edw17003@byui.edu>
2023-10-26 21:19:54 -0600

    added front end to test oauth

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const dotenv = require('dotenv')
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const exphbs = require('express-handlebars')
 
-app
-  .use(cors())
-  .use(express.json())
-  .use(express.urlencoded({ extended: true }))
-  .use('/', require('./routes'));
+dotenv.config()
+require('./config/passport')(passport)
 
 const db = require('./models');
+
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
@@ -22,7 +25,66 @@ db.mongoose
     process.exit();
   });
 
+const app = express();
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
+
+// app.use(
+//   methodOverride(function (req, res) {
+//     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+//       let method = req.body._method
+//       delete req.body._method
+//       return method
+//     }
+//   })
+// )
+
+app.use(session({ 
+    secret: 'test', 
+    resave: false, 
+    saveUninitialized: false,
+    store: MongoStore.create({mongoUrl: process.env.MONGODB_URI,}),
+}))
+
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+} = require('./helpers/hbs')
+
+app.engine(
+  '.hbs',
+  exphbs({
+    helpers: {
+      formatDate,
+      stripTags,
+      truncate,
+      editIcon,
+      select,
+    },
+    defaultLayout: 'main',
+    extname: '.hbs',
+  })
+)
+app.set('view engine', '.hbs')
+
+app.use(passport.initialize())
+app.use(passport.session());
+
+
+
+// Routes
+app.use('/', require('./routes'))
+app.use('/auth', require('./routes/auth'))
+// app.use('/stories', require('./routes/stories'))
+
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,26 @@
 const routes = require('express').Router();
 const user = require('./user');
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 routes.use('/', require('./swagger'));
 routes.use('/users', user);
-routes.use(
-  '/',
-  (docData = (req, res) => {
-    let docData = {
-      documentationURL: 'https://nathanbirch.github.io/nathan-byui-api-docs',
-    };
-    res.send(docData);
+routes.get('/', ensureGuest, (req, res) => {
+  res.render('login', {
+    layout: 'login',
   })
-);
+})
+
+routes.get('/dashboard', ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ user: req.user.id }).lean()
+    res.render('dashboard', {
+      name: req.user.firstName,
+      stories,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
+})
 
 module.exports = routes;
